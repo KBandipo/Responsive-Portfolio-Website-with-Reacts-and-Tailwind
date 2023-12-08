100ab4d7b397bd4a7f1ce2335b2ff00fd48840be
KBandipo~ <keneebandipo@gmail.com>
2023-12-09 00:41:12 +0100

    Import Link from  react-router-dom and change anchor tag to Link (#15)

--- src/assets/components/NavBar.jsx
+++ src/assets/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 // import ToggleButton from "./ToggleButton";
 // import { Link } from "react-router-dom";
 function NavBar() {
@@ -28,9 +29,9 @@ function NavBar() {
                 d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
               />
             </svg>
-            <a href="#home" className="nav-link">
+            <Link to="/" className="nav-link">
               Home
-            </a>
+            </Link>
           </li>
           <li>
             <svg
@@ -48,9 +49,9 @@ function NavBar() {
               />
             </svg>
 
-            <a href="#skills" className="nav-link">
+            <Link to="/skills" className="nav-link">
               Skills
-            </a>
+            </Link>
           </li>
           <li>
             <svg
@@ -67,9 +68,9 @@ function NavBar() {
                 d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
               />
             </svg>
-            <a href="#about" className="nav-link">
+            <Link to="/about" className="nav-link">
               About
-            </a>
+            </Link>
           </li>
           <li>
             <svg
@@ -86,9 +87,9 @@ function NavBar() {
                 d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
               />
             </svg>
-            <a href="#contact" className="nav-link">
+            <Link to="/contact" className="nav-link">
               Contact
-            </a>
+            </Link>
           </li>
           <li>
             <svg
@@ -106,9 +107,9 @@ function NavBar() {
               />
             </svg>
 
-            <a href="#projects" className="nav-link">
+            <Link to="/projects" className="nav-link">
               Projects
-            </a>
+            </Link>
           </li>
         </ul>
         <div className="sm:hidden">
