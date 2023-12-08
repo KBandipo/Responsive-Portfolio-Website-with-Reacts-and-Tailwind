ae8ff63fb89aeae9b96751ed3890386647f3eb8b
KBandipo~ <keneebandipo@gmail.com>
2023-12-09 00:42:52 +0100

    Route all the pages of the website (#15)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./assets/components/Home";
+import Contact from "./assets/components/Contact";
+import Projects from "./assets/components/Projects";
+import Skills from "./assets/components/Skills";
+import About from "./assets/components/About";
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
