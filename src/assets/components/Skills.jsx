771778d6cb12486087811f8ece115831d1901de9
KBandipo~ <keneebandipo@gmail.com>
2023-12-17 04:17:07 +0100

    Add image to the skill section (#8)

--- src/assets/components/Skills.jsx
+++ src/assets/components/Skills.jsx
@@ -1,11 +1,22 @@
-import MainLayout from "../../Layout/MainLayout"
+import MainLayout from "../../Layout/MainLayout";
 
 function Skills() {
   return (
     <>
-    <MainLayout>Skills</MainLayout>
+      <MainLayout>
+        <div className="mx-auto h-[96vh] max-w-md overflow-hidden rounded-xl bg-white pt-20 shadow-md md:max-w-[50%]">
+          <img
+            src="/images/ken1.jpg"
+            alt="Bandipo Kehinde's Picture"
+            className="h-[50%] w-[50%] rounded-[10%] object-cover shadow-lg"
+          />
+          <div className="w-[80%]">
+            <h2 className="">Frontend web developer, this are my skills:</h2>
+          </div>
+        </div>
+      </MainLayout>
     </>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
